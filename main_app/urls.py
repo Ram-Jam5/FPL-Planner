162b66a863197fe13cef104338093deb9531ce77
Ram-Jam5 <edgeillusive@gmail.com>
2024-08-22 12:28:00 -0500

    create team detail page and static elments.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
      path('', views.home, name='home'),
      path('about/', views.about, name='about'),
-     path('teams/', views.teams_index, name='teams-index')
+     path('teams/', views.teams_index, name='teams-index'),
+     path('teams/<int:teams_id>/', views.teams_detail, name='teams-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import  Team
 
 # Create your views here.
 def home(request):
@@ -9,24 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def teams_index(request):
+    teams = Team.objects.all() 
     return render(request, 'teams/index.html', { 'teams':teams })
 
-class Team:
-    def __init__(self, name, goalkeeper ,defender1, defender2, defender3, defender4, midfielder1, midfielder2, midfielder3, forward1, forward2, forward3):
-        self.name = name
-        self.goalkeeper = goalkeeper
-        self.defender1 = defender1
-        self.defender2 = defender2
-        self.defender3 = defender3
-        self.defender4 = defender4
-        self.midfielder1 = midfielder1
-        self.midfielder2 = midfielder2
-        self.midfielder3 = midfielder3
-        self.forward1 = forward1
-        self.forward2 = forward2
-        self.forward3 = forward3
-        
-teams = [
-    Team('Team 1', 'Vicarrio', 'Van Dijk', 'Alexander-Arnold', 'Pedro Porro', 'Saliba', 'Kevin De Bruyne', 'Bruno Fernandes', 'Cole Palmer', 'Erling Haaland', 'Alexander Isak', 'Ivan Toney'),
-    Team('Team 2', 'Martinez', 'Burn', 'Alexander-Arnold', 'Gvardiol', 'Pedro Porro', 'Jota', 'Salah', ' Saka', 'Isak', 'Wood', 'Haaland'),
-]
+def teams_detail(request, teams_id):
+    team = Team.objects.get(id=teams_id)
+    return render(request, 'teams/detail.html', {'team':team})
