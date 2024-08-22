ac2d456a5777d53abb5fb11d8a8d6431bd5b25c1
Ram-Jam5 <edgeillusive@gmail.com>
2024-08-22 10:16:43 -0500

    added template and dummy data for some teams and team index page. modified some css elements and logo.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
      path('', views.home, name='home'),
      path('about/', views.about, name='about'),
+     path('teams/', views.teams_index, name='teams-index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,26 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def teams_index(request):
+    return render(request, 'teams/index.html', { 'teams':teams })
+
+class Team:
+    def __init__(self, name, goalkeeper ,defender1, defender2, defender3, defender4, midfielder1, midfielder2, midfielder3, forward1, forward2, forward3):
+        self.name = name
+        self.goalkeeper = goalkeeper
+        self.defender1 = defender1
+        self.defender2 = defender2
+        self.defender3 = defender3
+        self.defender4 = defender4
+        self.midfielder1 = midfielder1
+        self.midfielder2 = midfielder2
+        self.midfielder3 = midfielder3
+        self.forward1 = forward1
+        self.forward2 = forward2
+        self.forward3 = forward3
+        
+teams = [
+    Team('Team 1', 'Vicarrio', 'Van Dijk', 'Alexander-Arnold', 'Pedro Porro', 'Saliba', 'Kevin De Bruyne', 'Bruno Fernandes', 'Cole Palmer', 'Erling Haaland', 'Alexander Isak', 'Ivan Toney'),
+    Team('Team 2', 'Martinez', 'Burn', 'Alexander-Arnold', 'Gvardiol', 'Pedro Porro', 'Jota', 'Salah', ' Saka', 'Isak', 'Wood', 'Haaland'),
+]
